31f3d8788a5a2fa6902ca0631fa0fae4137be6a7
thecodegoddess <thecodegodess17@gmail.com>
2016-07-01 23:31:57 -0700

    i hate this

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ var http, director, cool, bot, router, server, port;
 http        = require('http');
 director    = require('director');
 cool        = require('cool-ascii-faces');
-bot         = require('./bot-a.js');
-
-// myBot = new bot();
+bot         = require('./bot.js');
 
 router = new director.http.Router({
   '/' : {
     post: bot.respond,
-    // post: myBot.respond,
     get: ping
   }
 });
@@ -31,7 +28,6 @@ port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
-	console.log('PING');
   this.res.writeHead(200);
   this.res.end("Hey, I'm Cool Guy.");
 }
