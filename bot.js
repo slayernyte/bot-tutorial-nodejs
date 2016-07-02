3999e9470d762f90828ee8e07eea16f8e4415d92
thecodegoddess <thecodegodess17@gmail.com>
2016-07-01 21:53:50 -0700

    new branch

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond () {
 	var request = JSON.parse(this.req.chunks[0]), botRegex = /^\/cool guy$/;
 
-	if (request.text && (botRegex.test(request.text) || quick(request.text)) ) {
+	if (request.text && (botRegex.test(request.text) ||quick(request.text)) ) {
 		this.res.writeHead(200);
 		postMessage();
 		this.res.end();
